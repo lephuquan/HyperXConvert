51063bdac3c6b0490037b1da5d5dc898f3d67115
Le Phu Quan <80085752+lephuquan@users.noreply.github.com>
2025-08-07 15:23:06 +0700

    chore(ngrok): [HXC-099] Add ngrok access permission (#41)
    * chore(gitignore-and-banner): [HXC-099] Add banner and update gitignore
    
    * chore(gitignore): [HXC-099] Add gitignore
    
    * chore(ngrok): [HXC-099] Add ngrok access permission

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,9 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
     port: 3000,
+    allowedHosts: ['.ngrok-free.app'], // Cho phép domain từ ngrok
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
