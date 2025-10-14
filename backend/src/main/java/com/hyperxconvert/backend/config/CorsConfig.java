51256080abc0624892ae58b479f25681b09f891f
Le Phu Quan <lephuquan.dev@gmail.com>
2025-10-14 17:28:21 +0700

    chore(CORS): [HXC-144] Fix CORS error

--- backend/src/main/java/com/hyperxconvert/backend/config/CorsConfig.java
+++ backend/src/main/java/com/hyperxconvert/backend/config/CorsConfig.java
@@ -31,16 +31,14 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // chỉ cho API - Chỉ cho phép đi vào /api
-                        .allowedOrigins(
-                                "https://hyperxconverter.com",
-                                "https://www.hyperxconverter.com"
-                        )
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("https://*.hyperxconverter.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(false) // Chưa dùng có cookie/token
+                        .allowCredentials(false)
                         .maxAge(3600);
             }
         };
     }
+
 }
